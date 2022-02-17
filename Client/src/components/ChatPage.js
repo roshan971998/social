14d964bcf50988df97cf1f79c1f2a52b656eb278
ROSHAN KUMAR <rajr4350@gmail.com>
2022-02-17 17:37:16 +0530

    Add files via upload

---
+++ Client/src/components/ChatPage.js
@@ -0,0 +1,39 @@
+// import React from "react";
+
+// const Chatpage = () => {
+//   return (
+//     <div>
+//       <div className="chat-messages">
+//         {messages.map((message, index) => (
+//           <div
+//             className={
+//               message.self ? "chat-bubble self-chat" : "chat-bubble other-chat"
+//             }
+//             key={index}
+//           >
+//             {message.content}
+//           </div>
+//         ))}
+//       </div>
+
+//       <div className="chat-footer">
+//         <input
+//           type="text"
+//           value={typedMessage}
+//           onChange={(event) =>
+//             this.setState({ typedMessage: event.target.value })
+//           }
+//         />
+//         <button
+//           type="button"
+//           className="btn btn-success"
+//           onClick={this.handleSubmit}
+//         >
+//           Send
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Chatpage;
---
+++ Client/src/components/FriendList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { FriendsListItem } from "./";
+
+const FriendsList = (props) => {
+  return (
+    <div className="friends-list">
+      <div className="header">Friends</div>
+
+      {props.friends && props.friends.length === 0 && (
+        <div className="no-friends">No friends found!</div>
+      )}
+      <div className="friend-list-body">
+        {props.friends &&
+          props.friends.map((friend) => (
+            <FriendsListItem friend={friend.to_user} key={friend._id} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default FriendsList;
