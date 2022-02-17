4268e570ea481e2cb80c52af46bcaac41d8a037d
ROSHAN KUMAR <rajr4350@gmail.com>
2022-02-17 17:40:16 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,92 @@
+const http = require("http");
+const express = require("express");
+// const socketio = require("socket.io");
+const { Server } = require("socket.io");
+const { get_Current_User, user_Disconnect, join_User } = require("./dummyUser");
+
+const app = express();
+const httpServer = http.createServer(app);
+// const io = socketio(server);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+//initializing the socket io connection
+io.on("connection", (socket) => {
+  console.log("socket connected with ", socket.id);
+  socket.on("joinRoom", ({ username, roomname }) => {
+    //* create user
+    const p_user = join_User(socket.id, username, roomname);
+    console.log(socket.id, "=id");
+    socket.join(p_user.room);
+
+    //display a welcome message to the user who have joined a room
+    socket.emit("message", {
+      userId: p_user.id,
+      username: p_user.username,
+      text: `Welcome ${p_user.username}`,
+    });
+
+    //displays a joined room message to all other room users except that particular user
+    socket.broadcast.to(p_user.room).emit("message", {
+      userId: p_user.id,
+      username: p_user.username,
+      text: `${p_user.username} has joined the chat`,
+    });
+  });
+
+  //user sending message
+  socket.on("chat", (text) => {
+    //gets the room user and the message sent
+    const p_user = get_Current_User(socket.id);
+
+    io.to(p_user.room).emit("message", {
+      userId: p_user.id,
+      username: p_user.username,
+      text: text,
+    });
+  });
+
+  //when the user exits the room
+  socket.on("disconnect", () => {
+    //the user is deleted from array of users and a left room message displayed
+    const p_user = user_Disconnect(socket.id);
+
+    if (p_user) {
+      io.to(p_user.room).emit("message", {
+        userId: p_user.id,
+        username: p_user.username,
+        text: `${p_user.username} has left the chat`,
+      });
+    }
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("hello from server");
+});
+// app.use(express());
+httpServer.listen(5000, () => {
+  console.log("started at 5000...");
+});
+
+// const cors = require("cors");
+// const corsOptions = {
+//   origin: "*",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+
+// app.use(cors(corsOptions));
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+//   next();
+// });
